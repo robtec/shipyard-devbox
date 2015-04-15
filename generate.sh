ba9994646f9b3dab9b45196ff61b94b33a985b75
Tom Barlow <tomwbarlow@gmail.com>
2015-04-15 15:29:50 +0100

    Manually copy certs, as docker-machine on linux doesn't currently support shared folders
    Signed-off-by: Tom Barlow <tomwbarlow@gmail.com>

--- generate.sh
+++ generate.sh
@@ -14,6 +14,11 @@ if [ -n "${http_proxy+1}" ]; then
 fi
 DISCOVERY_ID=$(docker run -e http_proxy -e https_proxy -e no_proxy --rm swarm create)
 
+docker-machine ssh $ACTIVE_MACHINE 'sudo mkdir /certs && chmod -R 777 /certs'
+cat $DOCKER_CERT_PATH/cert.pem | docker-machine ssh $ACTIVE_MACHINE 'cat > /certs/cert.pem'
+cat $DOCKER_CERT_PATH/key.pem | docker-machine ssh $ACTIVE_MACHINE 'cat > /certs/key.pem'
+cat $DOCKER_CERT_PATH/ca.pem | docker-machine ssh $ACTIVE_MACHINE 'cat > /certs/ca.pem'
+
 sed -e "s/\${DEVBOX_IP}/$DEVBOX_IP/" -e "s/\${DISCOVERY_ID}/$DISCOVERY_ID/" docker-compose.yml.template > docker-compose.yml
 
 echo "# Docker host: https://$DEVBOX_IP:2376"
